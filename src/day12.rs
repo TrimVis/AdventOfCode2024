5f92cb590fab6ffe9ac7a373ef3cd2b7f90c3a3c
Pasquale Jordan <29759576+TrimVis@users.noreply.github.com>
2024-12-14 00:23:56 +0100

    Merged implementations together

--- src/day12.rs
+++ src/day12.rs
@@ -1,18 +1,10 @@
 use std::{collections::HashSet, fs};
 
+use itertools::Itertools;
+
 const DIRS: [(i64, i64); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
-const DIRS_WDIAG: [(i64, i64); 8] = [
-    (0, -1),
-    (1, 0),
-    (0, 1),
-    (-1, 0),
-    (1, 1),
-    (-1, -1),
-    (-1, 1),
-    (1, -1),
-];
-const INPUT_FILE: &str = "inputs/day12.test";
-// const INPUT_FILE: &str = "inputs/day12.input";
+// const INPUT_FILE: &str = "inputs/day12.test";
+const INPUT_FILE: &str = "inputs/day12.input";
 
 pub fn solve() {
     println!("Part 1: {}", solve_p1());
@@ -21,61 +13,43 @@ pub fn solve() {
 
 #[allow(dead_code)]
 struct Region {
-    id: char,
+    id: String,
     map: Vec<Vec<bool>>,
     visited: Vec<Vec<bool>>,
 }
 
 impl Region {
-    fn from_map(map: &Vec<Vec<char>>, id: char) -> Region {
+    fn from_map(map: &Vec<Vec<String>>, id: String) -> Region {
         let map: Vec<Vec<bool>> = map
             .iter()
-            .map(|row| row.iter().map(|&e| e == id).collect())
+            .map(|row| row.iter().map(|e| *e == id).collect())
             .collect();
-        let visited: Vec<Vec<bool>> = vec![vec![false; map[0].len() + 2]; map.len() + 2];
+        let visited: Vec<Vec<bool>> = vec![vec![false; map[0].len()]; map.len()];
 
         Region { id, map, visited }
     }
 
-    fn traverse(&mut self, pos: (usize, usize)) -> (u64, u64) {
-        let curr_pos = self.map[pos.1][pos.0];
-        let mut area = curr_pos as u64;
-        let mut perimeter = !curr_pos as u64;
-
-        if !curr_pos {
-            return (area, perimeter);
-        }
-        if self.visited[pos.1][pos.0] {
-            return (0, 0);
-        }
-        self.visited[pos.1][pos.0] = true;
-
-        for dir in DIRS {
-            let pos = (pos.0 as i64 + dir.0, pos.1 as i64 + dir.1);
-            if self.oobounds(pos) {
-                perimeter += 1;
-                continue;
-            }
-
-            let (narea, nperimeter) = self.traverse((pos.0 as usize, pos.1 as usize));
-            area += narea;
-            perimeter += nperimeter;
-        }
-
-        (area, perimeter)
-    }
-
-    fn get_price(&mut self) -> u64 {
+    fn get_price(&mut self, discount_edges: bool) -> u64 {
         let mut price = 0;
         for y in 0..self.map.len() {
             for x in 0..self.map[0].len() {
-                if !self.visited[y][x] {
-                    let (area, perimiter) = self.traverse((x, y));
-                    let iprice = area * perimiter;
+                if let Some((area, perimiter, no_sides)) = self.traverse((x as i64, y as i64)) {
+                    let iprice = if !discount_edges {
+                        area * perimiter
+                    } else {
+                        area * no_sides
+                    };
+                    if iprice > 0 {
+                        println!(
+                            "Region {} has a intermediate price result of {} (area: {}, no_sides: {}, perimiter: {})",
+                            self.id, iprice, area, no_sides, perimiter
+                        );
+                    }
                     price += iprice;
                 }
             }
         }
+        println!("Region {} has a price of {}", self.id, price);
         price
     }
 
@@ -87,105 +61,86 @@ impl Region {
     }
 
     fn check_visited(&mut self, pos: (i64, i64)) -> bool {
-        if pos.1 < -1
-            || pos.1 > self.map.len() as i64
-            || pos.0 < -1
-            || pos.0 > self.map[0].len() as i64
-        {
-            return true;
+        self.oobounds(pos) || {
+            let pos = (pos.1 as usize, pos.0 as usize);
+            let res = self.visited[pos.1][pos.0];
+            self.visited[pos.1][pos.0] = true;
+            res
         }
-        let pos = ((pos.1 + 1) as usize, (pos.0 + 1) as usize);
-
-        let res = self.visited[pos.1][pos.0];
-        self.visited[pos.1][pos.0] = true;
-        res
     }
 
     fn check_pos(&self, pos: (i64, i64)) -> bool {
-        if !self.oobounds(pos) {
-            self.map[pos.1 as usize][pos.0 as usize]
-        } else {
+        if self.oobounds(pos) {
             false
+        } else {
+            self.map[pos.1 as usize][pos.0 as usize]
         }
     }
 
     fn check_dpos(&self, pos: (i64, i64), dir: (i64, i64)) -> bool {
-        let pos = (pos.0 as i64 + dir.0, pos.1 as i64 + dir.1);
-        self.check_pos(pos)
+        self.check_pos((pos.0 as i64 + dir.0, pos.1 as i64 + dir.1))
     }
 
-    fn traverse_discounted(&mut self, pos: (i64, i64)) -> Option<(u64, u64)> {
-        if self.check_visited(pos) {
+    fn traverse(&mut self, pos: (i64, i64)) -> Option<(u64, u64, u64)> {
+        if self.check_visited(pos) || !self.check_pos(pos) {
             return None;
         }
 
         let curr_pos = self.check_pos(pos);
         let mut area = curr_pos as u64;
-        let mut sides = DIRS
+        let mut perimeter = DIRS
             .iter()
-            .zip(DIRS.iter().cycle().skip(1))
-            .filter(|&(&d0, &d1)| {
-                if curr_pos {
-                    !self.check_dpos(pos, d0) && !self.check_dpos(pos, d1)
-                } else {
-                    self.check_dpos(pos, d0) && self.check_dpos(pos, d1)
-                }
-            })
+            .filter(|&dir| !self.check_dpos(pos, *dir))
             .count() as u64;
-        if sides > 0 {
-            println!(
-                "{}: Position {:?} ({}) is a edge {}",
-                self.id, pos, curr_pos, sides
-            );
-        }
+
+        let mut sides = if curr_pos {
+            DIRS.iter()
+                .zip(DIRS.iter().cycle().skip(1))
+                .filter(|&(&d0, &d1)| {
+                    let d_combined = (d0.0 + d1.0, d0.1 + d1.1);
+                    let is_outward_edge = !self.check_dpos(pos, d0)
+                        && !self.check_dpos(pos, d1)
+                        && !self.check_dpos(pos, d_combined);
+                    let is_inward_edge = self.check_dpos(pos, d0)
+                        && self.check_dpos(pos, d1)
+                        && !self.check_dpos(pos, d_combined);
+                    is_outward_edge || is_inward_edge
+                })
+                .count() as u64
+        } else {
+            0
+        };
 
         if curr_pos {
-            for dir in DIRS_WDIAG {
+            for dir in DIRS {
                 let pos = (pos.0 + dir.0, pos.1 + dir.1);
                 // println!("{:?}", pos);
 
-                if let Some((narea, nsides)) = self.traverse_discounted(pos) {
+                if let Some((narea, nperimeter, nsides)) = self.traverse(pos) {
                     area += narea;
                     sides += nsides;
+                    perimeter += nperimeter;
                 }
             }
         }
 
-        Some((area, sides))
-    }
-
-    fn get_discounted_price(&mut self) -> u64 {
-        let mut price = 0;
-        for y in 0..self.map.len() {
-            for x in 0..self.map[0].len() {
-                if let Some((area, no_sides)) = self.traverse_discounted((x as i64, y as i64)) {
-                    // Finally compute the discounted price
-                    let iprice = area * no_sides;
-                    price += iprice;
-                    if iprice > 0 {
-                        println!(
-                            "Region {} has a intermediate price result of {} (area: {}, no_sides: {})",
-                            self.id, price, area, no_sides
-                        );
-                    }
-                }
-            }
-        }
-        println!("Region {} has a price of {}", self.id, price);
-        price
+        Some((area, perimeter, sides))
     }
 }
 
 pub fn solve_p1() -> u64 {
     let content = fs::read_to_string(INPUT_FILE).expect("Could not read input file");
 
-    let map: Vec<Vec<char>> = content.lines().map(|l| l.chars().collect()).collect();
-    let ids: HashSet<&char> = HashSet::from_iter(map.iter().flatten());
+    let map: Vec<Vec<String>> = content
+        .lines()
+        .map(|l| l.chars().map(|c| c.to_string()).collect())
+        .collect();
+    let ids: HashSet<&String> = HashSet::from_iter(map.iter().flatten());
 
     let mut total_price = 0;
-    for id in ids {
-        let mut region = Region::from_map(&map, *id);
-        let price = region.get_price();
+    for id in ids.iter().sorted_by_key(|v| v.to_string()) {
+        let mut region = Region::from_map(&map, (*id).clone());
+        let price = region.get_price(false);
         total_price += price;
     }
 
@@ -195,13 +150,16 @@ pub fn solve_p1() -> u64 {
 pub fn solve_p2() -> u64 {
     let content = fs::read_to_string(INPUT_FILE).expect("Could not read input file");
 
-    let map: Vec<Vec<char>> = content.lines().map(|l| l.chars().collect()).collect();
-    let ids: HashSet<&char> = HashSet::from_iter(map.iter().flatten());
+    let map: Vec<Vec<String>> = content
+        .lines()
+        .map(|l| l.chars().map(|c| c.to_string()).collect())
+        .collect();
+    let ids: HashSet<&String> = HashSet::from_iter(map.iter().flatten());
 
     let mut total_price = 0;
-    for id in ids {
-        let mut region = Region::from_map(&map, *id);
-        let price = region.get_discounted_price();
+    for id in ids.iter().sorted_by_key(|v| v.to_string()) {
+        let mut region = Region::from_map(&map, (*id).clone());
+        let price = region.get_price(true);
         total_price += price;
     }
 
